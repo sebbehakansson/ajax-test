f9a82f0bd3d35693891ea36ed8046a042820b310
sebbehakansson <sebastian.sh74@gmail.comm>
2023-01-17 11:40:32 +0100

    start fils

--- script.ts
+++ script.ts
@@ -1,1 +1,20 @@
-ECHO is on.
+const url = new URL('https://api.thecatapi.com/v1/images/search?limit=10');
+
+let showCats = document.querySelector('cats-picture') as HTMLParagraphElement;
+
+const p = fetch(url)
+    .then(Response => {
+        return Response.json();
+    }).then(catsArray => {
+        logCat(catsArray);
+    });
+
+function logCat(cats: any) {
+     showCats = cats[0];
+}
+
+
+
+
+
+

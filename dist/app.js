03de4b6ca8e300716f864965ee6e68c1d1037986
sebbehakansson <sebastian.sh74@gmail.comm>
2023-01-18 11:37:57 +0100

    final product

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,16 @@
 "use strict";
 let url = new URL('https://api.thecatapi.com/v1/images/search?limit=10');
 let showCats = document.querySelector('#cats-picture');
-let btn1 = document.querySelector('next-btn');
-let btn2 = document.querySelector('pre-btn');
-const p = fetch(url)
-    .then(Response => {
-    return Response.json();
-}).then(catsArray => {
-    logCat(catsArray);
+let btn1 = document.getElementById('next-btn');
+let btn2 = document.getElementById('pre-btn');
+btn1.addEventListener('click', function () {
+    const p = fetch(url);
+    p.then(Response => {
+        return Response.json();
+    }).then(catsArray => {
+        logCat(catsArray);
+    });
 });
 function logCat(catsArray) {
     showCats.src = catsArray[0].url;
 }
-btn1 === null || btn1 === void 0 ? void 0 : btn1.addEventListener('click', function () {
-    btn1.innerHTML = "hej";
-});
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,19 @@
 let url: any = new URL('https://api.thecatapi.com/v1/images/search?limit=10');
 
 let showCats: any = document.querySelector('#cats-picture') as HTMLImageElement;
-let btn1 = document.querySelector('next-btn') as HTMLButtonElement;
-let btn2 = document.querySelector('pre-btn') as HTMLButtonElement;
+let btn1 = document.getElementById('next-btn') as HTMLButtonElement;
+let btn2 = document.getElementById('pre-btn') as HTMLButtonElement;
 
-const p = fetch(url)
-    .then(Response => {
+btn1.addEventListener('click', function(){
+    const p = fetch(url);
+    p.then(Response => {
         return Response.json();
     }).then(catsArray => {
+        
         logCat(catsArray);
     });
-
-function logCat(catsArray: any) {
-     showCats.src = catsArray[0].url;
-}
-
-btn1?.addEventListener('click', function() {
 });
 
-
-
-
+function logCat(catsArray: any) {
+    showCats.src = catsArray[0].url;
+}
